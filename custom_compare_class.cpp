a8d6327a14cc4a2fb06d9f3d7beb118f8b432f6a
Brilliant-Developer1 <email2obail@gmail.com>
2023-07-25 23:46:56 +0600

    first commit

---
+++ custom_compare_class.cpp
@@ -0,0 +1,120 @@
+// #include<bits/stdc++.h>
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <cassert>
+#include <cctype>
+#include <cerrno>
+#include <cfloat>
+#include <ciso646>
+#include <climits>
+#include <clocale>
+#include <cmath>
+#include <complex>
+#include <csetjmp>
+#include <csignal>
+#include <cstdarg>
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <cwchar>
+#include <cwctype>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <random>
+#include <regex>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <strstream>
+#include <tuple>
+#include <typeinfo>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <valarray>
+#include <vector>
+
+// #include<bits/stdc++.h>
+using namespace std;
+
+class Student
+{
+public:
+    string name;
+    int roll;
+    int marks;
+
+    Student(string name, int roll, int marks)
+    {
+        this->name = name;
+        this->roll = roll;
+        this->marks = marks;
+    }
+};
+class cmp
+{
+public:
+    bool operator()(Student a, Student b)
+    {
+
+        if (a.marks > b.marks) // mark soto theke boro
+            return true;
+        else if (a.marks < b.marks)
+        {
+            return false;
+        }
+        else
+        {
+            if (a.roll > b.roll)
+                return true;
+            else
+                return false;
+        }
+    }
+};
+int main()
+{
+    int n;
+    cin >> n;
+    priority_queue<Student, vector<Student>, cmp> pq;
+    for (int i = 0; i < n; i++)
+    {
+        string name;
+        int roll, marks;
+        cin >> name >> roll >> marks;
+        Student obj(name, roll, marks);
+        pq.push(obj);
+    }
+
+    while (!pq.empty())
+    {
+        cout << pq.top().name << " " << pq.top().roll << " " << pq.top().marks << endl;
+        pq.pop();
+    }
+
+    return 0;
+}
---
+++ priority_queue.cpp
@@ -0,0 +1,92 @@
+// #include<bits/stdc++.h>
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <cassert>
+#include <cctype>
+#include <cerrno>
+#include <cfloat>
+#include <ciso646>
+#include <climits>
+#include <clocale>
+#include <cmath>
+#include <complex>
+#include <csetjmp>
+#include <csignal>
+#include <cstdarg>
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <cwchar>
+#include <cwctype>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <random>
+#include <regex>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <strstream>
+#include <tuple>
+#include <typeinfo>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <valarray>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    // Heap sort, insert korar shomoy sort kore insert kora jay
+
+    // priority_queue<int> pq; // maxHeap Priority qoueue, max element top e thakbe
+    priority_queue<int, vector<int>, greater<int>> pq; // minHeap Priority queue, min element top e thakbe
+    while (true)
+    {
+        int c;
+        cin >> c;
+        if (c == 0)
+        {
+            int x;
+            cin >> x;
+            pq.push(x); // O(logN)
+        }
+        else if (c == 1)
+        {
+            pq.pop(); // O(logN)
+        }
+        else if (c == 2)
+        {
+            cout << pq.top() << endl; // O(1)
+        }
+        else
+            break;
+    }
+
+    return 0;
+}
